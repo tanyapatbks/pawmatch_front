24011d92100e2170e2ecb16864ea961cb5f946f1
koraphat <koraphat.lum@gmail.com>
2024-11-05 18:29:45 +0700

    fix styling on matching page

--- src/app/(pet Match Service)/matching/page.tsx
+++ src/app/(pet Match Service)/matching/page.tsx
@@ -1,26 +1,46 @@
-'use client'
-import { useParams } from 'next/navigation'
-import PageBar from '@/components/PageBar'
-import TopMenu from '@/components/TopMenu'
-import { Button } from "@/components/ui/button"
-import { Card,CardContent, CardDescription } from '@/components/ui/card'
+"use client";
+import { useParams } from "next/navigation";
+import PageBar from "@/components/PageBar";
+import TopMenu from "@/components/TopMenu";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription } from "@/components/ui/card";
 
-
-export default function Page(){
-    const params = useParams()
-    const mockDatav0=['dog','cat','fish','bird','bear']
-    return <div className="h-screen bg-white flex flex-col items-center pb-10 space-b-[64px]">
-            <TopMenu />
-            <PageBar name="Matching" />
-            <div className='w-[80%] '>
-            {mockDatav0.map((item,index) => {
-                return <Card key={index} className='mb-3'>  
-                <CardContent key={index} className='flex flex-row items-center relative py-5 rounded-xl border-[3px] border-rose-950 px-10'>
-                    <CardDescription key={index} className='text-2xl text-rose-950'>From: {item}</CardDescription>
-                    <Button key={index} className="hover:bg-rose-500 absolute right-10 bg-rose-950" onClick={() => alert(params)}>Detail</Button>
-                </CardContent>
-            </Card>})
-            }
+export default function Page() {
+  const params = useParams();
+  const mockDatav0 = ["dog", "cat", "fish", "bird", "bear"];
+  return (
+    <div className="h-screen bg-white flex flex-col items-center space-y-[32px]">
+      <TopMenu />
+      <PageBar name="Matching" />
+      <div className="w-[80%] flex flex-col space-y-[24px]">
+        {mockDatav0.map((item, index) => {
+          return (
+            <Card key={index} className="">
+              <CardContent
+                key={index}
+                className="flex flex-row apace-x-[16px] items-center py-[16px] rounded-[24px] border-[3px] border-rose-600 px-[32px]"
+              >
+                <CardDescription
+                  key={index}
+                  className="text-[32px] text-rose-950 w-[20%] inline-block "
+                >
+                  From: {item}
+                </CardDescription>
+                <div className="flex flex-row justify-end w-full">
+                  {/* flex flox-row space-x-[8px] px-[24px] py-[8px] justify-center items-center rounded-[32px] text-[24px] bg-white border-[2.5px] border-rose-600 hover:bg-rose-200 hover:border-rose-700 active:bg-rose-300 active:border-rose-800 */}
+                  <Button
+                    key={index}
+                    className="bg-white text-[16px] border-[2.5px] border-rose-600 hover:bg-rose-200 hover:border-rose-700 active:bg-rose-300 active:border-rose-800 text-rose-950 px-[24px] py-[8px]"
+                    onClick={() => alert(params)}
+                  >
+                    Detail
+                  </Button>
                 </div>
-            </div>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
