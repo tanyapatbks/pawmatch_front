4c599912f4626e0b057947b9d750b8d1c8c3812c
koraphat <koraphat.lum@gmail.com>
2024-11-05 17:53:40 +0700

    improve button

--- src/components/Button/ButtonType1Small.tsx
+++ src/components/Button/ButtonType1Small.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
 
 export default function ButtonType1Small({
-    name,
-    pathURL
+  name,
+  pathURL,
 }: {
-    name: string;
-    pathURL: string;
+  name: string;
+  pathURL: string;
 }) {
-    return (
-        <Link href={pathURL}>
-            <div className="flex justify-center items-center w-[64px] h-8 rounded-md text-sm bg-white border-[2.5px] border-rose-600 hover:bg-rose-200">
-
-                <div className="">{name}</div>
-
-            </div>
-        </Link>
-    );
+  return (
+    <Link href={pathURL}>
+      <div className="flex flox-row space-x-[8px] px-[24px] py-[8px] justify-center items-center rounded-[32px] text-[16px] bg-white border-[2.5px] border-rose-600 hover:bg-rose-200 hover:border-rose-700 active:bg-rose-300 active:border-rose-800">
+        <div className="">{name}</div>
+      </div>
+    </Link>
+  );
 }
