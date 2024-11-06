096cf6c44393f6a4cb11d801e469b8ddc9ae8f91
koraphat <koraphat.lum@gmail.com>
2024-11-06 10:05:58 +0700

    add lib functions: 1

---
+++ src/lib/petService/getPet.ts
@@ -0,0 +1,15 @@
+export default async function getPet(pid: string, token: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_GATEWAY}/api/pets/${pid}`,
+    {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to Fetch Pets");
+  }
+  return await response.json();
+}
